8d8f47521b2dec465efacbc68ab7b63f4d0b7b82
eosam2003 <142361337+eosam2003@users.noreply.github.com>
2024-03-12 20:54:38 -0300

    Update Atividade7.java

--- Javacore3/src/atividade07/Atividade7.java
+++ Javacore3/src/atividade07/Atividade7.java
@@ -3,13 +3,19 @@ package atividade07;
 import java.util.Scanner;
 
 public class Atividade7 {
-	public static void main(String[] args) {
+	   public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
 
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Digite a Temperatura em graus Celsius:");
-		   float grausCelsius=sc.nextFloat();
-		   float grausFahrenheit=(9*grausCelsius+160)/5;
-		System.out.println("O valor da temperatura convertida é:"+grausFahrenheit+ "F");
-	sc.close();
-	}
+	        System.out.print("Digite o raio da lata de óleo (em centímetros): ");
+	        double raio = sc.nextDouble();
+
+	        System.out.print("Digite a altura da lata de óleo (em centímetros): ");
+	        double altura = sc.nextDouble();
+
+	        double volume = Math.PI * Math.pow(raio, 2) * altura;
+
+	        System.out.println("O volume da lata de óleo é: " + volume + " centímetros cúbicos.");
+
+	        sc.close();
+	    }
 }
